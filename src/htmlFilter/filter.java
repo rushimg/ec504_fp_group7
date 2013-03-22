327c1b1fdc525d63ea22ec5e123b738bf6f0bf86
TongLiu08 <lmzltlmzlt01@gmail.com>
2013-03-21 20:58:57 -0400

    Test the egit plugin of eclipse, netbeans git sucks

---
+++ src/htmlFilter/filter.java
@@ -0,0 +1,17 @@
+package htmlFilter;
+
+/**1. filter input html text to get useful content text.
+ * 2. parse useful text to words and count their frequencies.
+ * 3. Use huffman coding to encode the useful text (after 1,2 work well)
+ * 
+ * @author Tong Liu
+ *
+ */
+public class filter {
+	/**
+	 * constructor
+	 */
+	filter() {
+		
+	}
+}
