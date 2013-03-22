159667db5661b343d84702f4717d32c08f397213
neal <naizhaozhang@wireless1x-155-41-73-150.bu.edu>
2013-03-22 17:22:56 -0400

    s

--- src/dataStruct/StoreAndSearch.java
+++ src/dataStruct/StoreAndSearch.java
@@ -1,6 +1,28 @@
 package dataStruct;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map
+
+
 public class StoreAndSearch {
+	/* define data type*/
+	
+	
+	
+	
+	/*define method for store */
+	
+	
+	
+	
+	
+	
+	/*define method for search*/
+	
+	
+	
+	
+	
 
 }
-ss 
