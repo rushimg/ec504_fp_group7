07daa397c0dd2814d71231a127ae73bfcb47cd78
neal <naizhaozhang@181-31-41-155-wireles1x.bu.edu>
2013-03-22 18:11:49 -0400

    frame

--- src/dataStruct/StoreAndSearch.java
+++ src/dataStruct/StoreAndSearch.java
@@ -1,28 +1,47 @@
 package dataStruct;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map
+import java.util.*;
 
-
-public class StoreAndSearch {
+import DS.StroreAndSearch.Data;
+public class StroreAndSearch {
 	/* define data type*/
+	private String WordStore,WordSearch;
 	
+    private HashMap<String, Data> Map = new HashMap<String,Data>();
+
+	private ArrayList<Character> Dic1=new ArrayList<Character>();
+	private ArrayList<Character> Dic2=new ArrayList<Character>();
+	private ArrayList<Character> Dic3=new ArrayList<Character>();
+	private ArrayList<HashMap<String,Data>> Dic4=new ArrayList<HashMap<String,Data>>();
+	
+	private class Data{
+		private int freq;
+		private int Index;
+	}
 	
 	
 	
 	/*define method for store */
 	
-	
+   public void Store(String WordStore,Data Info){
+	   
+	   
+   }
 	
 	
 	
 	
 	/*define method for search*/
 	
+	public int Search(String WordSearch){
+		
+		
+		
+		
+		return 
+	}
 	
 	
 	
-	
-
 }
+
