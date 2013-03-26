d499dfd06ab0f53b38279d4c6041cedb484ce61e
Tong Liu <lmzltlmzlt01@gmail.com>
2013-03-26 00:19:00 -0400

    optimize filter implementaion to show more accurate results

--- src/htmlFilter/filter.java
+++ src/htmlFilter/filter.java
@@ -1,12 +1,16 @@
 package htmlFilter;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-/**1. filter input html text to get useful content text.
+/**
+ * 1. filter input HTML text to get useful content text.
  * 2. parse useful text to words and count their frequencies.
- * 3. Use huffman coding to encode the useful text (after 1,2 work well)
+ * 3. Use Huffman coding to encode the useful text (after 1,2 work well)
  * 
  * @author Tong Liu
  *
@@ -23,15 +27,62 @@ public class filter {
         inputHTML = htmlText;
     }
     
-    /**use a simple regex expression to test first
+    /**
+     * use several complicated regex expression to filter script, style and html tags
      * 
      * @return - text after filtering
      */
     public String filterToText() {
-        return inputHTML.replaceAll("<[^>]*>", "");
+        String outputStr = inputHTML;
+        String scriptRegex = "<[\\s]*?script[^>]*?>[\\s\\S]*?<[\\s]*?\\/[\\s]*?script[\\s]*?>";    //define regex expression of script
+        String styleRegex = "<[\\s]*?style[^>]*?>[\\s\\S]*?<[\\s]*?\\/[\\s]*?style[\\s]*?>";       //define regex expression of style
+        String htmlRegex = "<[^>]+>";                                                              //define regex expression of html
+
+        //filter script tags
+        Pattern scriptPattern = Pattern.compile(scriptRegex, Pattern.CASE_INSENSITIVE);
+        Matcher scriptMatcher = scriptPattern.matcher(outputStr);
+        outputStr = scriptMatcher.replaceAll("");
+
+        //filter style tags
+        Pattern stylePattern = Pattern.compile(styleRegex, Pattern.CASE_INSENSITIVE);
+        Matcher styleMatcher = stylePattern.matcher(outputStr);
+        outputStr = styleMatcher.replaceAll("");   
+
+        //filter style tags
+        Pattern htmlPattern = Pattern.compile(htmlRegex, Pattern.CASE_INSENSITIVE);
+        Matcher htmlMatcher = htmlPattern.matcher(outputStr);
+        outputStr = htmlMatcher.replaceAll("");    //space problem still needs to be solved, like: E</span>dit >> E dit after parsing
+        
+        //filter others
+        outputStr = outputStr.replaceAll("&nbsp;", "");
+        outputStr = outputStr.replaceAll("&lt;", "");
+        outputStr = outputStr.replaceAll("&copy;", "");
+        
+        return outputStr;
     }
     
-    /**parse the filtered text with "space" - DEC 32 in ascii
+    /**
+     * Test whether the character input is part of a useful word
+     * 
+     * @param inputChar - the character to be tst
+     * @return - whether the character is useful for a word to count, that is, number or letter
+     */
+    public boolean isUseful(char inputChar) {
+        if ((inputChar >= '0') && (inputChar <= '9')) {
+            return true;       //is number
+        }
+        if ((inputChar >= 'a') && (inputChar <= 'z')) {
+            return true;       //is capital letter
+        }
+        if ((inputChar >= 'A') && (inputChar <= 'Z')) {
+            return true;       //is lower letter
+        }
+        
+        return false;
+    }
+    
+    /**
+     * parse the filtered text with "space" - DEC 32 in ascii
      * 
      * @param filteredText - text to be parsed 
      */
@@ -39,9 +90,9 @@ public class filter {
         String subStr;      //word parsed
         int strLen = filteredText.length();     //length of filteredText
         for (int i = 0;i < strLen;i++) {
-            if (filteredText.charAt(i) != ' ') {
+            if (isUseful(filteredText.charAt(i))) { //judge for useful information
                 int subStrEnd = i + 1;          //end index of the word
-                while (filteredText.charAt(subStrEnd) != ' ') {     //looking for the end of the subString
+                while (isUseful(filteredText.charAt(subStrEnd))) {     //looking for the end of the subString
                     if (subStrEnd == strLen - 1) {      //to avoid indexOutOfBoundException
                         subStr = filteredText.substring(i, subStrEnd);  //edge case - at the end of the string
                         putIntoHashmap(subStr);
@@ -57,7 +108,8 @@ public class filter {
         }
     }
     
-    /**Store the word parsed into hash map and update frequency
+    /**
+     * Store the word parsed into hash map and update frequency
      * 
      * @param word - the word parsed to put into hash map
      */
@@ -70,8 +122,8 @@ public class filter {
         }
     }
     
-    /**Print all the words and their frequencies from hash map
-     * 
+    /**
+     * Print all the words and their frequencies from hash map
      */
     public void printHashMap() {
         Iterator<Map.Entry<String, Integer>> freqIter = freq.entrySet().iterator();
@@ -79,8 +131,15 @@ public class filter {
             Map.Entry<String, Integer> tempEntry = freqIter.next();
             String tempWord = tempEntry.getKey();
             int tempFreq = tempEntry.getValue();
-            System.out.println(tempWord + ": " + tempFreq);
+            System.out.println(tempWord + " : " + tempFreq);
         }
+    }
+    
+    /**
+     * return hashmap freq containing words and frequency
+     */
+    public HashMap<String, Integer> getHashMap() {      
+        return freq;
     }
     
     public void encode(String input) {
