7f06a2cf4d08a6980e3a419ee2f32cbebe7ef59f
neal-zhang <zernikeznz@gmail.com>
2013-04-20 17:55:48 -0300

    Update TestSearch.java

--- src/dataStruct/TestSearch.java
+++ src/dataStruct/TestSearch.java
@@ -1,43 +1,60 @@
-package dataStruct;
-import java.io.IOException;
-import java.util.ArrayList;
+package dataStruct
+import java.io.*;
+import java.util.*;
+
 
 public class TestSearch {
 	//StoreAndSearch testData = new StoreAndSearch();
-	static String data1 = new String("thisistestOne");
+	static String data1 = new String("time");
 	static String data2 = new String("ThisisTestTwo");	
 	static String data3 = new String("sh");
 
 	/**
 	 * @param args
 	 * @throws IOException 
+	 * @throws ClassNotFoundException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		// TODO Auto-generated method stub
 		StoreAndSearch testData = new StoreAndSearch();
 		testData.Store(data1, 2, 90);
-		testData.Store(data1, 1,80);
-		testData.Store(data1, 3, 70);
+		testData.Store("ssss", 3, 80);
+		testData.Store("abcd", 2, 100);
+		testData.Store("bbcd", 3, 85);
+		testData.Store("ccdd", 1, 70);
+		
 		testData.Store(data2, 4, 60);
+		testData.Store(data2, 4, 70);
+		testData.Store(data2, 4, 80);
 		testData.Store(data2, 5, 50);
 		testData.Store(data2, 6, 40);
 		testData.Store(data3, 7, 30);
 		testData.Store(data3, 8, 20);
 		testData.Store(data3, 9, 10);
+		testData.Store(data3, 9, 101);
 		ArrayList<Integer> retData1=new ArrayList<Integer>();
 		ArrayList<Integer> retData2=new ArrayList<Integer>();
 		ArrayList<Integer> retData3=new ArrayList<Integer>();
-
-		 retData1 = testData.Search(data1);
-		 retData2 = testData.Search(data2);
-		 retData3 = testData.Search(data3);
+		
+		ObjectOutputStream outStream = new ObjectOutputStream(new FileOutputStream("DS.txt"));
+		outStream.writeObject(testData);
+		outStream.close();
+		
+		ObjectInputStream inStream = new ObjectInputStream(new FileInputStream("DS.txt"));
+		StoreAndSearch decoded = (StoreAndSearch) inStream.readObject();
+		
+		
+		 retData1 = decoded.Search(data1);
+		 
+		 retData2 = decoded.Search(data2);
+		 retData3 = decoded.Search(data3);
 
 		System.out.println(retData1);
-	   System.out.println(retData2);
+	    System.out.println(retData2);
 		System.out.println(retData3);
 
 
-		 
+		
 	}
 
 }
