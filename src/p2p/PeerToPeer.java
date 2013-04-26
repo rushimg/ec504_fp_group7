525b28ffa7a0f619a6ca231d5eaaccf84663a37d
MikhailAndreev <mikha.andreev@gmail.com>
2013-04-25 21:44:24 -0400

    GetGraphMethods

--- src/p2p/PeerToPeer.java
+++ src/p2p/PeerToPeer.java
@@ -1,4 +1,5 @@
 package p2p;
+
 import java.io.*;
 import java.net.*;
 import java.io.BufferedReader;
@@ -18,66 +19,152 @@ import javax.swing.JOptionPane;
 
 public class PeerToPeer {
 
-      private int numPeers;
-      private ArrayList<String> peerUrls;
+    private int numPeers;
+    private ArrayList<String> peerUrls;
 
-      public static void main(String[] args) throws Exception {
-          InetAddress IP=InetAddress.getLocalHost();
-          System.out.println("IP of my system is := "+IP.getHostAddress());
-          server();
- 
-      	Graph graphNetGraph = new Graph();
-      	graphNetGraph.addNode("HElLo", "By");
-		//outStream1.writeObject(graphNetGraph);
-//          client("128.197.127.66", graphNetGraph );
-      }
+    public static void main(String[] args) throws Exception {
+        InetAddress IP = InetAddress.getLocalHost();
+        System.out.println("IP of my system is := " + IP.getHostAddress());
+        getGraph();
 
-      public static void client(String ipAddress, Graph graph) throws UnknownHostException, IOException{
-            Socket socket1;
-            int portNumber = 1774;
-            socket1 = new Socket(ipAddress, portNumber);
-            BufferedReader br = new BufferedReader(new InputStreamReader(socket1.getInputStream()));
-            PrintWriter pw = new PrintWriter(socket1.getOutputStream(), true);
-           String read = "";
-           while(true) 
-           pw.println("sdahbfasd");
+
+        Graph graphNetGraph = new Graph();
+        graphNetGraph.addNode("HElLo", "By");
+        sendGraph("128.197.127.66", graphNetGraph,1774);
+    }
     
-      }
-      
-      public static void server() throws IOException{
-          int cTosPortNumber = 1775;
-            String str;
+    public static Graph intialize(String destinationIP) throws IOException{
+        InetAddress IP = InetAddress.getLocalHost();
+        String ip = IP.getHostAddress();
+        requestGraph(destinationIP, 1774);
+        try {
+            return getGraph();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;    
+    }
+    
+    public static void requestGraph(String ipAddress,int portNumber) throws IOException{
+        Socket socket1;
 
-            ServerSocket servSocket = null;
+        socket1 = new Socket(ipAddress, portNumber);
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                socket1.getInputStream()));
+        PrintWriter pw = new PrintWriter(socket1.getOutputStream(), true);
+        ObjectOutputStream outStream1 = new ObjectOutputStream(
+                socket1.getOutputStream());
+
+        outStream1.writeObject(true);
+
+        br.close();
+        pw.close();
+        outStream1.close();
+        socket1.close();
+    }
+
+    public static void sendGraph(String ipAddress, Graph graph, int portNumber)
+
+            throws UnknownHostException, IOException {
+        Socket socket1;
+        socket1 = new Socket(ipAddress, portNumber);
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                socket1.getInputStream()));
+        PrintWriter pw = new PrintWriter(socket1.getOutputStream(), true);
+        ObjectOutputStream outStream1 = new ObjectOutputStream(
+                socket1.getOutputStream());
+
+        outStream1.writeObject(graph);
+
+        br.close();
+        pw.close();
+        outStream1.close();
+        socket1.close();
+
+    }
+
+    public static void sendQuery(String ipAddress, String Query, int portNumber)
+
+            throws UnknownHostException, IOException {
+        Socket socket1;
+        socket1 = new Socket(ipAddress, portNumber);
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                socket1.getInputStream()));
+        PrintWriter pw = new PrintWriter(socket1.getOutputStream(), true);
+        ObjectOutputStream outStream1 = new ObjectOutputStream(
+                socket1.getOutputStream());
+
+        outStream1.writeObject(Query);
+
+        br.close();
+        pw.close();
+        outStream1.close();
+        socket1.close();
+
+    }
+
+    public static Graph getGraph() throws IOException {
+        int cTosPortNumber = 1774;
+        Graph str;
+
+        ServerSocket servSocket = null;
+        try {
+            servSocket = new ServerSocket(cTosPortNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+        Socket fromClientSocket = servSocket.accept();
+        PrintWriter pw = new PrintWriter(fromClientSocket.getOutputStream(),
+                true);
+
+        ObjectInputStream inStream1 = new ObjectInputStream(
+                fromClientSocket.getInputStream());
+
+        while (true) {
             try {
-                servSocket = new ServerSocket(cTosPortNumber);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
+                while ((str = (Graph) inStream1.readObject()) != null) {
+                    str.printGraph();
+                    return str;
+                            
+                }
+            } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
-            System.out.println("Waiting for a connection on " + cTosPortNumber);
+        }
 
-            Socket fromClientSocket = servSocket.accept();
-            PrintWriter pw = new PrintWriter(fromClientSocket.getOutputStream(), true);
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(fromClientSocket.getInputStream()));
-            while(true){
-            while ((str = br.readLine()) != null) {
-              System.out.println("The message: " + str);
-
-              if (str.equals("bye")) {
-                pw.println("bye");
-                break;
-              } else {
-                str = "Server returns " + str;
-                pw.println(str);
-              }
-            }
-            }
-          //  pw.close();
-          //  br.close();
-
-          //  fromClientSocket.close();
-      }
-      
     }
+
+    public static void getQuery() throws IOException {
+        int cTosPortNumber = 1774;
+
+        String str;
+        ServerSocket servSocket = null;
+        try {
+            servSocket = new ServerSocket(cTosPortNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+        Socket fromClientSocket = servSocket.accept();
+        PrintWriter pw = new PrintWriter(fromClientSocket.getOutputStream(),
+                true);
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                fromClientSocket.getInputStream()));
+
+
+        while (true) {
+            while ((str = br.readLine()) != null) {
+                // do query stuff
+            }
+        }
+
+    }
+    
+    public static void sendUrl(String ipAddress){
+        
+    }
+
+}
